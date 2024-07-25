dd866d88c37cf069cb41de20e8ad49c6020ee7ef
sagonzalez76 <sagonzalez76@misena.edu.co>
2024-07-25 15:03:59 -0500

    Conexion db modificada

--- src/app.js
+++ src/app.js
@@ -5,14 +5,6 @@ import cors from 'cors';
 
 const app = express();
 
-const corsOptions = {
-    origin: 'http://localhost:5173',  // Permitir solicitudes solo desde este origen
-    methods: ['POST'],                // Permitir solo métodos POST
-    allowedHeaders: ['Content-Type'], // Permitir solo el encabezado Content-Type
-};
-
-// Aplicar la configuración de CORS a todas las rutas
-app.use(cors(corsOptions));
 
 
 // Import routes
